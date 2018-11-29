6ad2f12c42c3a122bcf0ddcae25b92f4dad78d03
Hanna Östling <hannaoliviaostling@gmail.com>
2018-11-29 15:35:09 +0100

    Initial Commit

---
+++ God Jul/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  God Jul
+//
+//  Created by Hanna Östling on 2018-11-29.
+//  Copyright © 2018 Hanna Östling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
